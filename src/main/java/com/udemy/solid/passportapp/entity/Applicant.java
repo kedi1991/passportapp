5701ec8121498c80abf45eeab14413bbee5d4f4d
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-10-10 14:46:29 +0300

    Created a new applciant class

---
+++ src/main/java/com/udemy/solid/passportapp/entity/Applicant.java
@@ -0,0 +1,14 @@
+package com.udemy.solid.passportapp.entity;
+
+import java.time.LocalDate;
+
+public class Applicant {
+	/*
+	 * Our applicant details
+	 */
+
+	private String fName;
+	private String lName;
+	private LocalDate doB;
+	private String ssName;
+}
