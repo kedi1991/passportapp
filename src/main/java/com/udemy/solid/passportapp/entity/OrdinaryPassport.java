044bd3b2beb33fda4e1c130bedafe7bd845a7fa4
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-10-20 18:40:29 +0300

    Created the oridinary passport class

---
+++ src/main/java/com/udemy/solid/passportapp/entity/OrdinaryPassport.java
@@ -0,0 +1,12 @@
+package com.udemy.solid.passportapp.entity;
+
+
+public class OrdinaryPassport extends Passport{
+
+	public OrdinaryPassport(Applicant applicant, String passportNumber,
+			String type) {
+		super(applicant, passportNumber, type);
+	
+	}
+
+}
