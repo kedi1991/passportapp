4af846824fe49f6f233903dee1d16ab925bbf89c
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-10-20 18:41:23 +0300

    Created the minor passport class

---
+++ src/main/java/com/udemy/solid/passportapp/entity/MinorPassport.java
@@ -0,0 +1,12 @@
+package com.udemy.solid.passportapp.entity;
+
+
+public class MinorPassport extends Passport{
+
+	public MinorPassport(Applicant applicant, String passportNumber,
+			String type) {
+		super(applicant, passportNumber, type);
+	
+	}
+
+}
