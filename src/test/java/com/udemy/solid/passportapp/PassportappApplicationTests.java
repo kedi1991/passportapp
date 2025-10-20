1f31c2ebd367d7f2b3bfb66d10e6972ed0a01502
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-10-20 12:51:35 +0300

    Added all files to be tracked on git

---
+++ src/test/java/com/udemy/solid/passportapp/PassportappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.solid.passportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassportappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
