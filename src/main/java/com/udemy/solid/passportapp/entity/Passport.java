3eaf4904afc96c5f2d5b5cd08cc576dcce82da98
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-10-20 13:07:22 +0300

    Added a constructor to the passport class as well as getters and setters

--- src/main/java/com/udemy/solid/passportapp/entity/Passport.java
+++ src/main/java/com/udemy/solid/passportapp/entity/Passport.java
@@ -2,14 +2,70 @@ package com.udemy.solid.passportapp.entity;
 
 import java.time.LocalDate;
 
+import com.udemy.solid.passportapp.services.Validity;
+
+/**
+ * Our Passport class
+ */
 public abstract class Passport {
-	/**
-	 * Our Passport class
-	 */
 
 	private Applicant applicant;
 	private String PassportNumber;
 	private LocalDate issueDate;
 	private LocalDate expiryDate;
+	private Validity validy;
+	
+	public Passport(Applicant applicant, String passportNumber, LocalDate issueDate, LocalDate expiryDate,
+			Validity validy) {
+		super();
+		this.applicant = applicant;
+		PassportNumber = passportNumber;
+		this.issueDate = issueDate;
+		this.expiryDate = expiryDate;
+		this.validy = validy;
+	}
+
+	public Applicant getApplicant() {
+		return applicant;
+	}
+
+	public void setApplicant(Applicant applicant) {
+		this.applicant = applicant;
+	}
+
+	public String getPassportNumber() {
+		return PassportNumber;
+	}
+
+	public void setPassportNumber(String passportNumber) {
+		PassportNumber = passportNumber;
+	}
+
+	public LocalDate getIssueDate() {
+		return issueDate;
+	}
+
+	public void setIssueDate(LocalDate issueDate) {
+		this.issueDate = issueDate;
+	}
+
+	public LocalDate getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setExpiryDate(LocalDate expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+
+	public Validity getValidy() {
+		return validy;
+	}
+
+	public void setValidy(Validity validy) {
+		this.validy = validy;
+	}
+	
+	
+	
 	
 }
